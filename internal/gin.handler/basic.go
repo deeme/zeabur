38830f44dfb026c3afaa32c5defa3d3acd7f03b0
deeme <github.io@gmail.com>
2024-08-05 17:34:11 +0800

    更改提示

--- internal/gin.handler/basic.go
+++ internal/gin.handler/basic.go
@@ -1,13 +1,22 @@
 package handler
 
 import (
+	"bytes"
 	"chatgpt-adapter/internal/gin.handler/response"
 	"chatgpt-adapter/internal/plugin"
 	"chatgpt-adapter/logger"
 	"chatgpt-adapter/pkg"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -30,6 +39,7 @@ func Bind(port int, version, proxies string) {
 	})
 
 	route.GET("/", welcome(version))
+	route.POST("/encipher", encipher)
 	route.POST("/v1/chat/completions", completions)
 	route.POST("/v1/object/completions", completions)
 	route.POST("/proxies/v1/chat/completions", completions)
@@ -52,11 +62,77 @@ func Bind(port int, version, proxies string) {
 	}
 }
 
+func encipher(context *gin.Context) {
+	key := context.GetHeader("x-key")
+	if key == "" {
+		response.Error(context, -1, "请提供 `x-key` 请求头")
+		return
+	}
+
+	defer context.Request.Body.Close()
+	db, err := io.ReadAll(context.Request.Body)
+	if err != nil {
+		logger.Error(err)
+		response.Error(context, -1, err)
+		return
+	}
+
+	content, err := encrypt(key, string(db))
+	if err != nil {
+		logger.Error(err)
+		response.Error(context, -1, err)
+		return
+	}
+
+	context.String(http.StatusOK, content)
+}
+
+func encrypt(key string, content string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	ctx := pad(content)
+
+	db := make([]byte, aes.BlockSize+len(ctx))
+	iv := db[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	iToB := func(n int) []byte {
+		x := int32(n)
+		buffer := bytes.NewBuffer([]byte{})
+		_ = binary.Write(buffer, binary.BigEndian, x)
+		return buffer.Bytes()
+	}
+
+	cipher.NewCBCEncrypter(block, iv).CryptBlocks(db[aes.BlockSize:], ctx)
+	toBytes := iToB(len(content))
+	db = append(db, toBytes...)
+	return hex.EncodeToString(db), nil
+}
+
+func pad(content string) (in []byte) {
+	in = []byte(content)
+	contentL := len(content)
+
+	if remain := contentL % 16; remain != 0 {
+		contentL = contentL + 16 - remain
+		contentL = contentL - len(content)
+		for i := 0; i < contentL; i++ {
+			in = append(in, 0)
+		}
+	}
+	return
+}
+
 func whiteIPHandler(context *gin.Context) {
 	// 作用不大
 	slice := pkg.Config.GetStringSlice("white-addr")
 	if len(slice) != 0 {
-		addr := context.ClientIP()
+		addr := getIp(context)
 		if slices.Contains(slice, addr) {
 			context.Next()
 		} else {
@@ -65,6 +141,43 @@ func whiteIPHandler(context *gin.Context) {
 			context.Abort()
 		}
 	}
+}
+
+func getIp(context *gin.Context) (ip string) {
+	ip = context.ClientIP()
+	header := context.GetHeader("X-Ip-Token")
+	if header == "" {
+		return
+	}
+
+	slice := strings.Split(header, ".")
+	if len(slice) != 3 {
+		return
+	}
+
+	db, err := base64.StdEncoding.DecodeString(slice[1] + "==")
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	var obj map[string]interface{}
+	if err = json.Unmarshal(db, &obj); err != nil {
+		logger.Error(err)
+		return
+	}
+
+	value, ok := obj["ip"].(string)
+	if !ok {
+		return
+	}
+
+	if pkg.Config.GetString("x-addr") != ip {
+		return
+	}
+
+	ip = value
+	return
 }
 
 func proxiesHandler(proxies string) gin.HandlerFunc {
--- pkg/config.go
+++ pkg/config.go
@@ -2,8 +2,14 @@ package pkg
 
 import (
 	"bytes"
+	"crypto/cipher"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
 	"github.com/spf13/viper"
 	"embed"
+
+	"crypto/aes"
 )
 
 //go:embed config.yaml
@@ -11,7 +17,24 @@ var configFile embed.FS
 
 var (
 	Config *viper.Viper
+
+	keys = []string{
+		"white-addr",
+		"you.cookies",
+		"claude.cookies",
+		"coze.websdk.accounts",
+		"llm.token",
+	}
 )
+
+func InitConfig() {
+	//time.Sleep(3 * time.Second)
+	config, err := LoadConfig()
+	if err != nil {
+		panic(err)
+	}
+	Config = config
+}
 
 func LoadConfig() (*viper.Viper, error) {
 	data, err := configFile.ReadFile("config.yaml")
@@ -25,14 +48,52 @@ func LoadConfig() (*viper.Viper, error) {
 		return nil, err
 	}
 
+	c := os.Getenv("CIPHER")
+	if c == "" {
+		return vip, nil
+	}
+
+	newCipher, err := aes.NewCipher([]byte(c))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, key := range keys {
+		content := vip.GetString(key)
+		if content != "" {
+			var d any
+			d, err = decrypt(newCipher, content)
+			if err != nil {
+				return nil, err
+			}
+			vip.Set(key, d)
+		}
+	}
+
 	return vip, nil
 }
 
-func InitConfig() {
-	//time.Sleep(3 * time.Second)
-	config, err := LoadConfig()
+func decrypt(block cipher.Block, content string) (data any, err error) {
+	db, err := hex.DecodeString(content)
 	if err != nil {
-		panic(err)
+		return
 	}
-	Config = config
+
+	bToI := func(b []byte) int {
+		buffer := bytes.NewBuffer(b)
+		var x int32
+		_ = binary.Read(buffer, binary.BigEndian, &x)
+		return int(x)
+	}
+
+	iv := db[:aes.BlockSize]
+	contentL := bToI(db[len(db)-4:])
+	ctx := db[aes.BlockSize:]
+	cipher.NewCBCDecrypter(block, iv).CryptBlocks(ctx, ctx[:len(ctx)-4])
+	ctx = ctx[:contentL]
+
+	if json.Unmarshal(ctx, &data) != nil {
+		return string(ctx), nil
+	}
+	return
 }
